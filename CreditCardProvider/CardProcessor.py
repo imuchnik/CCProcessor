8d5582a50e16552c0f9ad4523129866424aa128e
Irina <muchnik.irina@gmail.com>
2016-03-10 23:34:02 -0800

    refactored out CC

--- CreditCardProvider/CardProcessor.py
+++ CreditCardProvider/CardProcessor.py
@@ -1,13 +1,15 @@
 import operator
 from decimal import Decimal
+from CreditCard import CreditCard
 
 CREDIT_ACTION = "Credit"
 
 CHARGE_ACTION = "Charge"
 
 DOLLAR_SIGN = "$"
-
 ERROR = "error"
+
+
 
 
 class CardProcessor:
@@ -16,27 +18,6 @@ class CardProcessor:
         self.card_activity = {}
         for input_line in input_lines:
             self.process_entry(input_line)
-
-    # From Wikipedia
-    def luhn_checksum(self, card_number):
-        """Check if card is Luhn10 checksum valid"""
-
-        def digits_of(n):
-            return [int(d) for d in str(n)]
-
-        digits = digits_of(card_number)
-        odd_digits = digits[-1::-2]
-        even_digits = digits[-2::-2]
-        checksum = 0
-        checksum += sum(odd_digits)
-        for d in even_digits:
-            checksum += sum(digits_of(d * 2))
-        return checksum % 10
-
-    def is_luhn_valid(self, card_number):
-        """Wrapper for checksum validation"""
-
-        return self.luhn_checksum(card_number) == 0
 
     def process_entry(self, entry):
         """ Process each line and take appropriate action"""
@@ -53,41 +34,26 @@ class CardProcessor:
         """ Add card to internal data structure, if the card number is valid"""
 
         action, name, cardNumber, limit = entry
-        balance = 0
-        # what happens if name is a duplicate.
-        if self.is_luhn_valid(cardNumber):
-            self.card_activity[name] = [cardNumber, Decimal(limit[1:]), balance]
-        else:
-            self.card_activity[name] = ERROR
+        self.card_activity[name] = CreditCard(name, cardNumber, limit)
+
 
     def process_transaction(self, entry):
         """ Process "Charge or Credit"""
 
-        if (self.is_valid_entry(entry)):
-            action, name, amount = entry
-            cardNumber, limit, balance = self.card_activity[name]
-            amount = Decimal(amount[1:])
-            if action == CHARGE_ACTION:
-                if not (balance + amount) > limit:
-                    total = balance + amount
-                else:
-                    return
-            elif action == CREDIT_ACTION:
-                total = balance - amount
+        action, name, amount = entry
+        amount = Decimal(amount[1:])
+        if action == CHARGE_ACTION:
+            self.card_activity[name].charge(amount)
 
-            self.card_activity[name] = [cardNumber, limit, total]
-        else:
-            pass
+        elif action == CREDIT_ACTION:
+            self.card_activity[name].credit(amount)
 
-    def is_valid_entry(self, entry):
-        """  Validate card number was valid when it was added    """
-
-        return self.card_activity[entry[1]] != ERROR
 
     def display_statement(self):
         """ Print the final statement """
 
         for entry in sorted(self.card_activity, key=operator.itemgetter(0)):
             name = entry
-            value = self.card_activity[entry] if self.card_activity[entry] == ERROR else DOLLAR_SIGN + str(self.card_activity[entry][2])
+            balance=str(self.card_activity[entry].get_balance())
+            value = ERROR if balance==ERROR else DOLLAR_SIGN + str(balance)
             print(name + ": " + value)
--- CreditCardProvider/CreditCard.py
+++ CreditCardProvider/CreditCard.py
@@ -1,31 +1,73 @@
+ERROR = "error"
+
+
 class CreditCard:
     """A consumer credit card."""
 
-    def init(self, customer, bank, card_number, limit):
+    def __init__(self, customer, card_number, limit):
         """Create a new credit card instance.The initial balance is zero. Customer the name of the customer (Lisa) and credit limit """
 
         self.customer = customer
-        self.card_number = card_number
-        self.limit = limit
-        self.balance = 0
+        if (self.is_luhn_valid(card_number)):
+            self.card_number = card_number
+            self.limit = limit
+            self.balance = 0
 
+        else:
+            self.card_number = ERROR
+            self.balance = ERROR
 
     def get_customer(self):
         """Return name of the customer."""
 
         return self.customer
 
-
     def get_account(self):
         """Return the card  number."""
         return self.card_number
-
 
     def get_limit(self):
         """Return current credit limit."""
         return self.limit
 
-
     def get_balance(self):
         """Return current balance."""
         return self.balance
+
+    def luhn_checksum(self, card_number):
+        """Check if card is Luhn10 checksum valid"""
+
+        def digits_of(n):
+            return [int(d) for d in str(n)]
+
+        digits = digits_of(card_number)
+        odd_digits = digits[-1::-2]
+        even_digits = digits[-2::-2]
+        checksum = 0
+        checksum += sum(odd_digits)
+        for d in even_digits:
+            checksum += sum(digits_of(d * 2))
+        return checksum % 10
+
+    def is_luhn_valid(self, card_number):
+        """Wrapper for checksum validation"""
+
+        return self.luhn_checksum(card_number) == 0
+
+    def charge(self, amount):
+        """ Adds charge to the credit card"""
+
+        if self.is_valid_entry() and not (self.balance + amount) > self.limit:
+                    self.balance = self.balance + amount;
+        else:
+            return
+
+    def credit(self, amount):
+        """Applies credit to the balance"""
+        if  self.is_valid_entry():
+            self.balance = self.balance - amount
+
+    def is_valid_entry(self):
+        """  Validate card number was valid when it was added    """
+        return self.card_number != ERROR
+
